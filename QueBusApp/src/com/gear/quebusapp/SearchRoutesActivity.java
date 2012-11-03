fa77939e106657953ae72f6450c331d9f420c070
Juan_Ramirez <j127dra@gmail.com>
2012-11-03 11:32:44 -0500

    Empty activities

---
+++ QueBusApp/src/com/gear/quebusapp/SearchRoutesActivity.java
@@ -0,0 +1,7 @@
+package com.gear.quebusapp;
+
+import android.app.Activity;
+
+public class SearchRoutesActivity extends Activity {
+
+}
---
+++ QueBusApp/src/com/gear/quebusapp/SeeRoutesActivity.java
@@ -0,0 +1,7 @@
+package com.gear.quebusapp;
+
+import android.app.Activity;
+
+public class SeeRoutesActivity extends Activity {
+
+}
